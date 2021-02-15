d19f8298b619e974a2f6d8555390f237f38748ce
Florianbracke <florianbracke@gmail.com>
2021-02-15 15:37:50 +0100

    add structure

---
+++ src/Todo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+export default function Todo({ todo }) {
+    return (
+        <div>
+            <label>
+                {todo.name}
+                <input type="checkbox" checked={todo.complete} />
+            </label>   
+        </div>
+    )
+}
+ 
